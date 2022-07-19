00550bb0a148f410d34b8976024eaf8eab0f4cbf
flash_hammad <flash_hammad@yahoo.co.in>
2022-07-19 19:41:27 +0530

    Graded Assignment 4 for Spring Boot Service

---
+++ employee-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into employee values (1,'emp1@gl.com','Hammad','Sala');
+insert into employee values (2,'emp2@gl.com','Sam','Kumar');
+insert into user values (1,'$2a$12$wFaqlQc2MThKjVXV7WWdVexy9.p2L/VrspP15nNOcsN0kvngJXUaq','hammad');
+insert into user values (2,'$2a$12$wFaqlQc2MThKjVXV7WWdVexy9.p2L/VrspP15nNOcsN0kvngJXUaq','sala');
+insert into role values (1,'ADMIN');
+insert into role values (2,'USER');
+insert into user_roles values (1,1);
+insert into user_roles values (2,2);
